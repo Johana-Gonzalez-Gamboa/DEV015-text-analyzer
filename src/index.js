9d30e93181715d399c23735b5b0003b4699ba0f3
JovyLab <JovyLab@MacBook-Pro-de-Johana.local>
2024-05-19 00:17:43 -0400

    subo cambios de estructura de html y funcion recuento de caracteres

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const textArea = document.querySelector('textarea[name="user-input"]');
+
+textArea.addEventListener(
+    "input",
+    (event) => {
+        const totalCaracteres = analyzer.getCharacterCount(event.target.value)
+        const lis = document.querySelectorAll('li');
+
+        lis[0].innerHTML = "Caracteres: " + totalCaracteres
+    }
+)
