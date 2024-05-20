20c8018cf187e9f9689c5320ea9685ad25a09cae
Johana-Gonzalez-Gamboa <jovita_ninda_19gatit@hotmail.com>
2024-05-20 19:18:53 -0400

    subo cambios de agregar funcionalidad recuento de palaras y recuento de caracteres sin espacios y puntuacion

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,20 +1,34 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    
+    const palabras = text.split(' ');
+    const totalPalabras = palabras.filter(palabra => palabra !== '');
+    return totalPalabras.length
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    
     return text.length
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    
+    const puntuacion = /[ /[.,;:!?¿¡()"'`´“”‘’\]{}<>\-—_…«»]/g;
+    const nuevoTexto = text.replace(puntuacion, "");
+    return nuevoTexto.length
   },
+
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
--- src/index.js
+++ src/index.js
@@ -7,9 +7,24 @@ const textArea = document.querySelector('textarea[name="user-input"]');
 textArea.addEventListener(
   "input",
   (event) => {
-    const totalCaracteres = analyzer.getCharacterCount(event.target.value)
     const lis = document.querySelectorAll('li');
 
+    // Logica para conteo de caracteres
+    const totalCaracteres = analyzer.getCharacterCount(event.target.value)
+
     lis[0].innerHTML = "Caracteres: " + totalCaracteres
+
+    // Logica para conteo de caracteres sin espacios y puntuacion
+    const totalCaracteresSinEspacios = analyzer.getCharacterCountExcludingSpaces(event.target.value)
+
+    lis[1].innerHTML = "Caracteres sin espacios y puntuacion: " + totalCaracteresSinEspacios
+
+    // Logica para conteo de palabras
+    const totalPalabras = analyzer.getWordCount(event.target.value)
+
+    lis[2].innerHTML ="Palabras: " + totalPalabras
+
+
   }
 )
+
